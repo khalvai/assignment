5a697e97f88e8b3d73b291baf50efce662b35968
khalvai <78291644+khalvai@users.noreply.github.com>
2024-07-26 23:01:24 +0330

    feat: product repository and its postgresql implementation is added.

--- src/Product/Application/Ports/Output/ProductRepository.ts
+++ src/Product/Application/Ports/Output/ProductRepository.ts
@@ -2,6 +2,7 @@ import Code from "src/Product/Domain/Code";
 import { Product } from "src/Product/Domain/Product";
 
 
+export const ProductRepository = Symbol('ProductRepository').valueOf();
 export interface ProductRepository {
 
     productExistsIn(codes: Code[]): Promise<boolean>
--- src/Product/Infrastructure/PostgresqlProductRepository.ts
+++ src/Product/Infrastructure/PostgresqlProductRepository.ts
@@ -1,18 +1,44 @@
+import { Injectable } from "@nestjs/common";
+import PrismaService from "src/Common/Infrastructure/Output/PrismaService";
 import { ProductRepository } from "src/Product/Application/Ports/Output/ProductRepository";
 import Code from "src/Product/Domain/Code";
 import { Product } from "src/Product/Domain/Product";
+import { ProductMapper } from "src/Product/Infrastructure/ProductMapper";
 
-
+@Injectable()
 export class PostgresqlProductRepository implements ProductRepository {
 
 
+    constructor(private readonly prisma: PrismaService, private readonly mapper: ProductMapper) { }
+
+
     async productExistsIn(codes: Code[]): Promise<boolean> {
-        throw new Error("Method not implemented.");
+        const stringCodes = codes.map(c => c.value)
+
+        const exists = await this.prisma.product.findMany({
+            where: {
+                code: {
+                    in: stringCodes
+                }
+            }
+        })
+        return exists.length > 0 ? true : false
+
     }
     async save(product: Product): Promise<void> {
-        throw new Error("Method not implemented.");
+        const productModel = this.mapper.toPersistent(product)
+        await this.prisma.product.create({
+            data: productModel
+        })
+        return
     }
     async saveAll(products: Product[]): Promise<void> {
-        throw new Error("Method not implemented.");
+
+        const productsModel = products.map(p => this.mapper.toPersistent(p))
+        await this.prisma.product.createMany({
+            data: productsModel
+        })
+
+        return
     }
 }
