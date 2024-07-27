52a7e819c2057f85cf2ccd09e818bd57c7631679
khalvai <78291644+khalvai@users.noreply.github.com>
2024-07-27 14:24:12 +0330

    feat: toDomain mapper is added to product mapper.

--- src/Product/Infrastructure/ProductMapper.ts
+++ src/Product/Infrastructure/ProductMapper.ts
@@ -1,7 +1,12 @@
 import { Injectable } from "@nestjs/common";
+import Code from "src/Product/Domain/Code";
+import Id from "src/Product/Domain/Id";
 import { Product } from "src/Product/Domain/Product";
+import { UserId } from "src/Product/Domain/UserId";
+import { Value } from "src/Product/Domain/Value";
 import { ProductReadModel } from "src/Product/Infrastructure/Models/ProductReadModel";
 import { ProductPersistenceModel } from "src/Product/Infrastructure/ProductPersistenceModel";
+import Name from "src/User/Domain/Name";
 
 
 @Injectable()
@@ -21,5 +26,18 @@ export class ProductMapper {
 
     toRead(persistenceModel: ProductPersistenceModel): ProductReadModel {
         return { code: persistenceModel.name, value: persistenceModel.value, name: persistenceModel.name }
+    }
+
+    toDomain(persistenceModel: ProductPersistenceModel): Product {
+
+        const product = new Product()
+        product.id = Id.fromValid(persistenceModel.id)
+        product.code = Code.createFromValid(persistenceModel.code)
+        product.name = Name.createFromValid(persistenceModel.name)
+        product.value = Value.createFromValid(persistenceModel.value)
+        product.userId = UserId.createFromValid(persistenceModel.userId)
+
+        return product
+
     }
 }
